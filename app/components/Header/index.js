edaae7382f5ffe3172c21bf8f09f3e83f6b0093c
wangyuan <Yuan@lendingcar.com>
2018-04-10 15:15:54 +0800

    add Menu UserDropdown Header component

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,25 +1,37 @@
 /**
 *
-* Header
+* Header Component
 *
 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'antd';
 
-import TranslatedMessage from 'components/TranslatedMessage';
-import messages from './messages';
+import Menu from 'components/Menu';
 import './header.scss';
 
-function Header() {
+function Header(props) {
+  const { logoSrc, menuItems } = props;
   return (
     <div className="header">
-      <TranslatedMessage messages={messages} messageId="header" />
+      <div className="page-wrap">
+        <Row type="flex" justify="space-between">
+          <Col>
+            <img src={logoSrc} alt="logo" />
+          </Col>
+          <Col>
+            <Menu menuItems={menuItems} />
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
 
 Header.propTypes = {
-
+  logoSrc: PropTypes.string,
+  menuItems: PropTypes.array,
 };
 
 export default Header;
--- app/components/Header/messages.js
+++ app/components/Header/messages.js
@@ -10,4 +10,28 @@ export default defineMessages({
     id: 'app.components.Header.header',
     defaultMessage: 'This is the Header component !',
   },
+  home: {
+    id: 'app.components.Header.home',
+    defaultMessage: 'Home',
+  },
+  crowdFund: {
+    id: 'app.components.Header.crowdFund',
+    defaultMessage: 'crowdFund',
+  },
+  language: {
+    id: 'app.components.Header.language',
+    defaultMessage: 'English',
+  },
+  signIn: {
+    id: 'app.components.Header.signIn',
+    defaultMessage: 'Sign In',
+  },
+  signUp: {
+    id: 'app.components.Header.signUp',
+    defaultMessage: 'Sign Up',
+  },
+  signOut: {
+    id: 'app.components.Header.signOut',
+    defaultMessage: 'Sign Out',
+  },
 });
