dc1961ee42d0d2ff8d9590ac4fd0011919a3bf28
peili <peili0413@gmail.com>
2018-04-28 09:42:48 +0800

    headliner

--- app/containers/HomePage/components/Headliner/index.js
+++ app/containers/HomePage/components/Headliner/index.js
@@ -5,36 +5,29 @@
 */
 
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import TranslatedMessage from 'components/TranslatedMessage';
-import Button from 'components/Button';
-import head from './assets/head.png';
+import image from './assets/react-boilerplate.png';
 import './style.scss';
 
 // const headlinerStyle = { backgroundImage: `url(${car})` };
 
-function Headliner(props) {
-  const { linkTo } = props;
+function Headliner() {
   return (<div className="head-liner">
     <div className="text">
       <h1 className="name">
-        <TranslatedMessage id="app.components.Headliner.name" />
+        <TranslatedMessage id="app.default.text1" />
       </h1>
       <div className="break-line"></div>
       <h2 className="title">
-        <TranslatedMessage id="app.components.Headliner.title" tagName="pre" />
-        <Button label="fundNow" type="primary" className="title-button" onClick={() => linkTo('/products')} />
+        <TranslatedMessage id="app.default.text2" tagName="pre" />
       </h2>
       <h3 className="sub-title">
-        <TranslatedMessage id="app.components.Headliner.subTitle" />
+        <TranslatedMessage id="app.default.text3" />
       </h3>
     </div>
-    <img className="head-image" src={head} alt="head-img" />
+    <img className="head-image" src={image} alt="alt" />
   </div>);
 }
-
-Headliner.propTypes = {
-  linkTo: PropTypes.func,
-};
 
 export default Headliner;
